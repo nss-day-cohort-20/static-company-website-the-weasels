d0614e3a6c29ee562c37f77a27f5a92aa3f5224b
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-05-30 11:57:31 -0500

    adding placeholder data to array

---
+++ main.js
@@ -0,0 +1,50 @@
+var products = [
+	{
+		name: "Work",
+	  	price: 100,
+	  	description: "Work description Placeholder",
+	  	imgUrl: "work image url Placeholder"
+	},
+	{
+		name : "School",
+		price : 200,
+		description : "School description Placeholder",
+		imgUrl : "school image url Placeholder"
+	},
+	{
+		name: "Wedding",
+		price: 300,
+		description: "Wedding description Placeholder",
+		imgUrl: "Wedding img url Placeholder"
+	},
+	{
+		name: "Funeral",
+		price: 400,
+		description: "Funeral description Placeholder",
+		imgUrl: "Funeral img url Placeholder"
+	},
+	{
+		name: "Kids Activity",
+		price: 500,
+		description: "Kids Activity description Placeholder",
+		imgUrl: "Kids Activity img url Placeholder"
+	},
+	{
+		name: "Bad Date",
+		price: 600,
+		description: "Bad Date description Placeholder",
+		imgUrl: "Bad Date img url Placeholder"
+	},
+	{
+		name: "In-Law Visit",
+		price: 700,
+		description: "In-Law Visit description Placeholder",
+		imgUrl: "In-Law Visit img url Placeholder"
+	},
+	{
+		name: "Parrents Visit",
+		price: 800,
+		description: "Parrents Visit description Placeholder",
+		imgUrl: "Parrents Visit img url Placeholder"
+	}
+];
